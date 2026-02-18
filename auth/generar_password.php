c9e49a8729b88e9e5e5f6480fee084c64318b9e9
chaustrex <cristianchaustre90@gmail.com>
2026-02-18 17:19:01 -0500

    refactor: Organizar estructura del proyecto - eliminar carpetas duplicadas y consolidar assets

--- auth/generar_password.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Script para generar contraseñas encriptadas
-$password = 'admin123';
-$hash = password_hash($password, PASSWORD_DEFAULT);
-
-echo "Contraseña: $password\n";
-echo "Hash: $hash\n";
-?>
